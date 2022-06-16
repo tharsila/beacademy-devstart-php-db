b84517362fce94be623425ff66cb4e5e90da45b4
tharsila <tharsilapaula@gmail.com>
2022-06-16 18:55:04 -0300

    altera nome da functions e add suas ações

--- modulo3/projeto-final/src/Controller/CategoryController.php
+++ modulo3/projeto-final/src/Controller/CategoryController.php
@@ -4,20 +4,47 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Connection\Connection;
+
 class CategoryController extends AbstractController
 {
-  public function listCategory(): void
-  {
-    parent::render('category/list');
+ public function listAction(): void
+ {
+
+  $con = Connection::getConnection();
+  $query = 'SELECT * FROM tb_category';
+  $result = $con->prepare($query);
+  $result->execute();
+  
+  parent::render('category/list', $result);
+ }
+
+ public function addAction(): void
+ {
+
+  if($_POST) {
+    $name = $_POST['name'];
+    $description = $_POST['description'];
+
+    $query = "INSERT INTO tb_category(name, description) VALUES('{$name}', '{$description}')";
+
+    $con = Connection::getConnection();
+
+    $result = $con->prepare($query);
+    $result->execute();
   }
 
-  public function addCategory(): void
-  {
-    parent::render('category/add');
-  }
+  parent::render('category/add');
+ }
 
-  public function editCategory(): void
-  {
-    parent::render('category/edit');
-  }
+ public function removeAction(): void
+ {
+  $id = $_GET['id'];
+  $con = Connection::getConnection();
+  $query = "DELETE FROM tb_category WHERE id={$id}";
+  $result = $con->prepare($query);
+  $result->execute();
+
+  echo 'Categoria excluida';
+ }
 }
