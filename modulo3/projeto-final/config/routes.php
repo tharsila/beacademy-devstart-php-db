29f50a411bafedbab0edfe4b8564c51b1c339b4a
tharsila <tharsilapaula@gmail.com>
2022-06-15 23:03:16 -0300

    add rotas

---
+++ modulo3/projeto-final/config/routes.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Controller\indexController;
+use App\Controller\ProductController;
+use App\Controller\CategoryController;
+
+function createRoute(string $controllerName, string $methodName){
+  return [
+    'controller' => $controllerName,
+    'method' => $methodName,
+  ];
+}
+
+$routes = [
+  '/' => createRoute(IndexController::class, 'indexAction' ),
+  '/produtos' => createRoute(ProductController::class, 'listAction'),
+  '/produtos/novo' => createRoute(ProductController::class, 'addAction'),
+  '/categorias' => createRoute(CategoryController::class, 'listAction'),
+  '/categorias/novo' => createRoute(CategoryController::class, 'addAction')
+];
