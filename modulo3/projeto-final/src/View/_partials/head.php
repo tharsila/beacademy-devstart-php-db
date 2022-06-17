20ba63603b58df00ae27ad9224c7b1ef68483533
tharsila <tharsilapaula@gmail.com>
2022-06-17 09:46:46 -0300

    add partials

---
+++ modulo3/projeto-final/src/View/_partials/head.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lojinha Virtual</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../../styles/style.css">
+  </head>
+  <body>
+    <header class="header-bg">
+      <div class="header container">
+        <a class="logo" href="/">Crud</a>
+        <nav>
+          <a href="/">Inicio</a>
+          <a href="/categorias">Categorias</a>
+          <a href="/produtos">Produtos</a>
+        </nav>
+      </div>
+
+    </header>
